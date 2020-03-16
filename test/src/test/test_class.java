f77ee7c65126e2726acd953f2219ad8d5f7fb2ed
Your Name <bjedpr1@naver.com>
2020-03-16 20:46:59 +0900

    commit data type

---
+++ test/src/test/test_class.java
@@ -0,0 +1,59 @@
+package test;
+
+public class test_class {
+	static public void main(String[] args) {
+		// print
+		System.out.println("Hello!");
+		
+		// This is comment
+		/* This is comment */
+		
+		// variable
+		String name = "moon";
+		int age = 20;
+		float tall = 180.1f;
+		char firstNameChar = 'm';
+		boolean marry = false;
+		
+		String firstStr = "mo";
+		String secondStr = "on";
+		
+		String mix_str = firstStr + secondStr;
+		
+		int small_int = 5;
+		int big_int = 10;
+		
+		int sum = small_int + big_int;
+		
+		int x=1, y=2, z=3;
+		
+		// data type
+		
+		/*
+		 * byte = 1byte
+		 * short = 2bytes
+		 * int = 4bytes
+		 * long = 8bytes
+		 * float = 4bytes
+		 * double = 8bytes
+		 * boolean = 1bit
+		 * char = 2bytes
+		 * */
+		
+		byte myByte = 0;
+		short myShort = 1;
+		int myInt = 2;
+		long myLong = 3L;
+		float myFloat = 3.1f;
+		double myDouble = 3.2d;
+		boolean myBool = true;
+		char myChar = 'm';
+		
+		
+		
+		
+		
+		
+	}
+
+}
